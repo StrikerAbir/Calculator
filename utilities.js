9b063ca4b77414982414318f01dbfcf2561fcb22
Abir Hasan <cse.170201063@gmail.com>
2022-08-18 06:16:11 +0600

    practice dom calculator 1. input text field that only takes number and operator 2. input field value function 3. operation function 4. set result in board function

---
+++ utilities.js
@@ -0,0 +1,39 @@
+// only number and operator are allowed in input
+function onlyNumberInput(event) {
+    const text = event.charCode;
+    if ((text >= 48 && text <= 57) || text == 47 || text == 42 || text == 43 || text == 45) {
+        return true;
+    } else {
+        return false;
+    }
+}
+
+// input field value
+function getInputFieldValue(inputFieldId) {
+    const inputText = document.getElementById(inputFieldId);
+    if (inputText != '') {
+        const inputValue = inputText.value;
+        return inputValue;
+    } else {
+        return 0;
+    }
+}
+
+// operations
+function operation(num1, num2, operation) {
+    if (operation == '+') {
+        return num1 + num2;
+    } else if (operation == '-') {
+        return num1 - num2;
+    } else if (operation == '*') {
+        return num1 * num2
+    } else if (operation == '/') {
+        return num1 / num2;
+    }
+}
+
+// set result in board
+function setResult(elementId,result) {
+    const element = document.getElementById(elementId);
+    element.innerText = `= ${result}`;
+}
